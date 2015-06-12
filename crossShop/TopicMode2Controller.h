b866d93ccdfdfba67778dbae1886d70e22f13209
mac <mac@fmwifi.local>
2015-06-12 17:23:18 +0800

    commit

---
+++ crossShop/TopicMode2Controller.h
@@ -0,0 +1,13 @@
+//
+//  TopicMode2Controller.h
+//  crossShop
+//
+//  Created by mac on 15/6/12.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicMode2Controller : UITableViewController
+
+@end
---
+++ crossShop/TopicMode2HeadCell.m
@@ -0,0 +1,23 @@
+//
+//  TopicMode2HeadCell.m
+//  crossShop
+//
+//  Created by mac on 15/6/12.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import "TopicMode2HeadCell.h"
+
+@implementation TopicMode2HeadCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
